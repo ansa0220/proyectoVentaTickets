f42eb23c429d2d19b17dbfa4a5a93bb44f65aac8
ansa0220 <116978051+ansa0220@users.noreply.github.com>
2022-12-11 23:48:55 -0500

    Update consultar_registro_test.py

--- consultar_registro_test.py
+++ consultar_registro_test.py
@@ -7,7 +7,7 @@ engine = generate_engine("testNew")
 from pprint import pprint
 import os
 
-
+import json
 # funcion para limpiar la pantalla para cualquier sistema
 def clear():
     # for windows
@@ -21,9 +21,13 @@ clear()
 with engine.connect() as conn:
     print("# Consultar registros de la tabla ticket con el id del punto de venta 3")
     result = conn.execute("SELECT * FROM ticket WHERE id_pdv = 3").mappings().all()
+    
     pprint(result)
 
-    print("# Obtener el total de las ventas de todos los tickets")
+    print("# Obtener el total de las ventas en tickets")
 
     result = conn.execute("SELECT SUM(precio_unitario) FROM ticket").mappings().all()
+    pprint(result)
+
+    result = conn.execute("select * from evento join testnew.show on evento.id_evento = show.id_evento where year(fecha) = 2022 ").mappings().all()
     pprint(result)
