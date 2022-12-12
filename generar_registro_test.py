a044ab713e41b4e3bf4a5488202fb360cfb8183a
Mr.Edison <85226841+EdisonYagual@users.noreply.github.com>
2022-12-11 19:12:59 -0500

    Actualización tablas
    Ingreso de registro para las tablas usuario, productor, administrador, cliente un total de 20 usuarios.

--- generar_registro_test.py
+++ generar_registro_test.py
@@ -12,36 +12,58 @@ from crear_tablas import usuario, administrador, productor, cliente, evento, ciu
 with engine.connect() as conn:
     conn.execute("commit")
     conn.execute(usuario.insert(), [
-        {'usuario': 'admin', 'correo': 'admin@admin.com', 'telefono': '0987654320', 'password': 'admin', 'direccion': 'Quito'},
-        {'usuario': 'productor', 'correo': 'productor@productor.com', 'telefono': '0927654320', 'password': 'productor', 'direccion': 'Quito'},
-        # genera 8 clientes
-        {'usuario': 'cliente1', 'correo': 'cliente1@gmail.com', 'telefono': '0987654321', 'password': 'cliente1', 'direccion': 'Quito'},
-        {'usuario': 'cliente2', 'correo': 'cliente2@gmail.com', 'telefono': '0987654322', 'password': 'cliente2', 'direccion': 'Quito'},
-        {'usuario': 'cliente3', 'correo': 'cliente3@gamil.com', 'telefono': '0987654323', 'password': 'cliente3', 'direccion': 'Quito'},
-        {'usuario': 'cliente4', 'correo': 'cliente4@gmail.com', 'telefono': '0987654324', 'password': 'cliente4', 'direccion': 'Quito'},
-        {'usuario': 'cliente5', 'correo': 'cliente5@gmail.com', 'telefono': '0987654325', 'password': 'cliente5', 'direccion': 'Quito'},
-        {'usuario': 'cliente6', 'correo': 'cliente6@gmail.com', 'telefono': '0987654326', 'password': 'cliente6', 'direccion': 'Quito'},
-        {'usuario': 'cliente7', 'correo': 'cliente7@gmail.com', 'telefono': '0987654327', 'password': 'cliente7', 'direccion': 'Quito'},
-        {'usuario': 'cliente8', 'correo': 'cliente8@gmail.com', 'telefono': '0987654328', 'password': 'cliente8', 'direccion': 'Quito'},
+        {'usuario': '1205', 'correo': 'rabad@hotmail.com', 'telefono': '0929032490', 'password': 'm#886N', 'direccion': 'WHYMPER E7-98 Y PASAJE DONOSO'},
+        {'usuario': '9364', 'correo': 'eacosta@gmail.com', 'telefono': '0925523190', 'password': '52$T3l', 'direccion': 'ALPALLANA E6-114 Y FRANCISCO FLOR'},
+        {'usuario': '1301', 'correo': 'iacosta@outloook.com', 'telefono': '0925698980', 'password': '3d#7Z0', 'direccion': '9 DE OCTUBRE N22-48 Y JERÓNIMO CARRIÓN'},
+        {'usuario': '5819', 'correo': 'vacosta@hotmail.com', 'telefono': '0956989890', 'password': 'j2A03@', 'direccion': '9 DE OCTUBRE N22-49 Y JERÓNIMO CARRIÓN'},
+        {'usuario': '1633', 'correo': 'jacosta@outloook.com', 'telefono': '0925056609', 'password': 'oG462^', 'direccion': 'WHYMPER E7-39 Y ALPALLANA'},
+        {'usuario': '8760', 'correo': 'wacurio@gmail.com', 'telefono': '0925698980', 'password': '8X$k22', 'direccion': '9 DE OCTUBRE N22-50 Y JERÓNIMO CARRIÓN'},
+        {'usuario': '9476', 'correo': 'bagila@hotmail.com', 'telefono': '0925698988', 'password': '1G^44x', 'direccion': '9 DE OCTUBRE N22-51 Y JERÓNIMO CARRIÓN'},
+        {'usuario': '6300', 'correo': 'maguinaga@outloook.com', 'telefono': '0925056666', 'password': 'Q920m!', 'direccion': 'WHYMPER E7-38 Y ALPALLANA'},
+        {'usuario': '5977', 'correo': 'taguirre@hotmail.com', 'telefono': '0925056607', 'password': 'q6&G69', 'direccion': 'WHYMPER E7-37 Y ALPALLANA'},
+        {'usuario': '6268', 'correo': 'raguirre@gmail.com', 'telefono': '0925698987', 'password': '8*5W7h', 'direccion': 'LA PRENSA 42-95 Y MARIANO ECHEVERRÍA SECTOR LA Y'},
+        {'usuario': '2024', 'correo': 'jose_aguagallo@hotmail.com', 'telefono': '0929618999', 'password': '7#9xL8', 'direccion': 'AV. UNIDAD NACIONAL ENTRE URUGUAY Y BOLIVIA SEGUNDO PISO'},
+        {'usuario': '8825', 'correo': 'maria_apolo@outlook.com', 'telefono': '0925715030', 'password': '5Ed8!0', 'direccion': 'ROCAFUERTE 1344 Y BOLIVAR PLAZA DE SANTO DOMINGO, EDIFICIO GEOMIL'},
+        {'usuario': '8157', 'correo': 'rodrigo_camacho@gmail.com', 'telefono': '0925881022', 'password': '230!Hj', 'direccion': 'REPUBLICA DEL SALVADOR N. 34 -183 Y SUIZA'},
+        {'usuario': '9218', 'correo': 'patricio_escobar@hotmail.com', 'telefono': '0938140123', 'password': '&133Wh', 'direccion': 'REPUBLICA DEL SALVADOR N. 34 -184 Y SUIZA'},
+        {'usuario': '1608', 'correo': 'alonso_insuasti@hotmail.com', 'telefono': '0938412345', 'password': '8@v89B', 'direccion': 'AV. JUAN JOSE PAEZ 233 Y ABELARDO MORAN'},
+        {'usuario': '6948', 'correo': 'ruben_gualle@outlook.com', 'telefono': '0926452530', 'password': 'u%66J5', 'direccion': 'REPUBLICA DEL SALVADOR N. 34 -186 Y SUIZA'},
+        {'usuario': '1856', 'correo': 'martha_rivera@outlook.com', 'telefono': '0939474400', 'password': '4@rZ08', 'direccion': 'CLEMENTE PONCE Y PIEDRAHITA'},
+        {'usuario': '5169', 'correo': 'martha_ona@gmail.com', 'telefono': '0922288166', 'password': '8%aW65', 'direccion': 'CALLE IMBABURA N7-59 Y OLMEDO - ANA MAC AULIFFE'},
+        {'usuario': '7169', 'correo': 'hector_jimenez@gmail.com', 'telefono': '0932981478', 'password': 'cV695#', 'direccion': 'AV. CANDIDO RADA ENTRE MANUELA CAÑIZARES Y SALINAS FRENTE A LA FISCALIA'},
+        {'usuario': '9983', 'correo': 'nely_teran@gmail.com', 'telefono': '0925881258', 'password': 'B5$y60', 'direccion': 'ROCAFUERTE 1344 Y BOLIVAR PLAZA DE SANTO DOMINGO, EDIFICIO GEOMIL'}
+        
     ])
 
     conn.execute(administrador.insert(), [
-        {'usuario': 'admin', 'cedula': '1234567890'}
+        {'usuario': '8760', 'cedula': '1720215134'},
+        {'usuario': '9476', 'cedula': '2100323860'},
+        {'usuario': '6300', 'cedula': '1725887093'},
+        {'usuario': '5977', 'cedula': '0603476599'},
+        {'usuario': '6268', 'cedula': '1716725336'}
     ])
 
     conn.execute(productor.insert(), [
-        {'RUC': '0234567890', 'nombre': 'productor', 'telefono': '0987654321', 'direccion': 'Quito', 'usuario': 'productor'}
+        {'usuario': '1205', 'RUC': '0103871307001', 'razon_social': 'razon_social01'},
+        {'usuario': '9364', 'RUC': '1714672274001', 'razon_social': 'razon_social02'},
+        {'usuario': '1301', 'RUC': '1713280491001', 'razon_social': 'razon_social03'},
+        {'usuario': '5819', 'RUC': '1002926093001', 'razon_social': 'razon_social04'},
+        {'usuario': '1633', 'RUC': '1718165622001', 'razon_social': 'razon_social05'}
+        
     ])
 
     conn.execute(cliente.insert(), [
-        {'usuario': 'cliente1', 'cedula': '1234567891'},
-        {'usuario': 'cliente2', 'cedula': '1234567892'},
-        {'usuario': 'cliente3', 'cedula': '1234567893'},
-        {'usuario': 'cliente4', 'cedula': '1234567894'},
-        {'usuario': 'cliente5', 'cedula': '1234567895'},
-        {'usuario': 'cliente6', 'cedula': '1234567896'},
-        {'usuario': 'cliente7', 'cedula': '1234567897'},
-        {'usuario': 'cliente8', 'cedula': '1234567898'}
+        {'usuario': '2024', 'cedula': '0600686166', 'nombres': 'JOSE HUMBERTO', 'apellidos': 'AGUAGALLO LASO'},
+        {'usuario': '8825', 'cedula': '0700897622', 'nombres': 'MARIA JOSEFA', 'apellidos': 'APOLO ORDOÑEZ'},
+        {'usuario': '8157', 'cedula': '0200400687', 'nombres': 'RODRIGO EFREN', 'apellidos': 'CAMACHO VARGAS'},
+        {'usuario': '9218', 'cedula': '0501169882', 'nombres': 'NELSON PATRICIO', 'apellidos': 'ESCOBAR RIVERA'},
+        {'usuario': '1608', 'cedula': '1000925089', 'nombres': 'ALONSO RODRIGO', 'apellidos': 'INSUASTI POZO'},
+        {'usuario': '6948', 'cedula': '1700456518', 'nombres': 'RUBEN ADALBERTO', 'apellidos': 'GUALLE QUISAGUANO'},
+        {'usuario': '1856', 'cedula': '1705779070', 'nombres': 'MARTHA DEL ROCIO', 'apellidos': 'RIVERA VELA'},
+        {'usuario': '5169', 'cedula': '1704977592', 'nombres': 'MARTHA MARIA DE LOURDES', 'apellidos': 'OÑA RAMOS'},
+        {'usuario': '7164', 'cedula': '1100072550', 'nombres': 'HECTOR MINOS', 'apellidos': 'JIMENEZ ROMERO'},
+        {'usuario': '9983', 'cedula': '1001402484', 'nombres': 'MARIA NELY', 'apellidos': 'TERAN VILLARREAL'}
+        
     ])
 
     conn.execute(evento.insert(), [
